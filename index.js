26a7e7bc5178fb20bbf06c7cee58f790de76bba7
maqiie <candiemp2580@gmai.com>
2022-11-27 14:45:32 +0300

    complete assignment

---
+++ index.js
@@ -0,0 +1,19 @@
+/*function divide(){
+const num1 = 2000
+const num2 = 100
+let divide = num1/num2
+const equal = 20
+console.log(num1/num2);
+}
+*/
+function divide () {
+    return 2000 / 100;
+}
+divide();
+
+const square = (y) => (y ** 2);
+
+let x = 4
+let y = 8
+
+const add = (x, y) => x + y;
